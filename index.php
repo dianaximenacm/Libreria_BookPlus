71f62203331d6914298b4f71212d0ca80197aa26
Diana Ximena Chitiva <dianaximenacm@gmail.com>
2020-04-28 12:38:35 -0500

    pages

--- index.php
+++ index.php
@@ -3,22 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Intro :: Clases PHP</title>
+    <title>Index</title>
 </head>
 <body>
-    <div id="login">
-        <form method="POST" action="control/usuarioControl.php?accion=login">
-            <input type="text" name="correo" id="correo" placeholder="Email" />
-            <br>
-            <input type="password" name="contrasena" id="contrasena" placeholder="Contraseña" />
-            <br>
-            <input type="submit" value="Iniciar Sesión" name="enter" />
-        </form>
-    </div>
-    <br><br>
-
-    <div id="registro">
-        <a href="registroForm.php">¿Eres Nuevo? <br> Regístrate!</a>
-    </div>
+    
 </body>
 </html>
--- modelo/class.libro.php
+++ modelo/class.libro.php
@@ -1,42 +1,6 @@
 <?php
 
-/* Creamos nuestro modelo de Producto a partir de una clase */
 
-require(__DIR__ . '/../config/class.Conexion.php');
 
-class Producto{
-
-    public function __construct(){
-
-    }
-
-    // método agregar Producto
-    public function agregarProducto($nombreProducto, $idUsuario){
-
-        $db = new Conexion();
-
-        # NOW() función propia de PHP que nos permite guardar en BD la fecha actual extraída desde el pc
-        $sql = "INSERT INTO productos (nombre, fecha, idUsuario) VALUES ('$nombreProducto', NOW(), '$idUsuario')";
-
-        $db->query($sql) ? header('Location: ../perfil.php?res=Producto_Agregado') : header('Location: ../perfil.php?res=error');
-    }
-
-    // método mostrar Productos
-    public function mostrarProductos(){
-        $db = new Conexion();
-
-        $sql = "SELECT p.*, u.* FROM productos AS p INNER JOIN usuarios AS u ON p.idUsuario = u.id";
-
-        $result = $db->query($sql);
-
-        if($result->num_rows > 0){
-            $row = mysqli_fetch_all($result);
-            return $row;
-        } else {
-            return 'error';
-        }
-    }
-
-}
 
 ?>
