767faad7f6623afd6685fcc0f729213a2dd1f501
mainhorse <52118001+mainhorse@users.noreply.github.com>
2020-05-02 20:56:31 -0500

    es la parte que controla los eventos como nuevo, actualizar, eliminar y mas con los libros.

--- control/libroControl.php
+++ control/libroControl.php
@@ -1,5 +1,59 @@
-<?php 
+<?php
 
+$accion = $_GET['accion'];
 
+switch($accion){
+    case 'nuevo':
+                 nuevoProducto();
+    break;
+    case 'modificar':
+                 modificarProducto();
+    break;
+    case 'categoria':
+                 categoria();
+    break;
+    case: 'subCategoria':
+                 subCategoria();
+    break;        
+    
+}
 
-?>
+function nuevoProducto(){
+    $producto = new Producto();
+    $nombre = $_POST[''];
+    $autor = $_POST[''];
+    $editorial = $_POST[''];
+    $existencias = $_POST[''];
+    $precio = $_POST[''];
+    $cate = $_POST[''];
+    $subCate = $_POST[''];
+    $estado = $_POST[''];
+    $result = $producto->nuevoProducto($nombre,$autor,$editorial,$existencias,$precio,$Cate,$subCate,$estado);
+    header('location: ../sudo.php?value=ProductoNuevo');
+}
+
+function modificarProducto(){
+    $producto = new Producto();
+    $idProducto = $_POST[''];
+    $columnaProducto = $_POST[''];
+    $nuevoValor = $_POST[''];
+    $producto->modificarProducto($idProducto,$columnaProducto,$nuevoValor);
+    header('location: ../sudo.php?value=modificarProducto');
+
+}
+
+function categoria(){
+    $producto = new Producto();
+    $nueCategoria = $_POST[''];
+    $estado = $_POST[''];
+    $producto->agregarCategoria($nueCategoria,$estado);
+}
+
+function subCategoria(){
+    $producto = new Producto();
+    $nueCategoria = $_POST[''];
+    $estado = $_POST[''];
+    $producto->agregarsubCategoria($nueCategoria,$estado);
+}
+
+>?
