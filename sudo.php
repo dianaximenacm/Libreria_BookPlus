d330b750e10e1d8c98e06e7709de9bcc60a35e77
mainhorse <52118001+mainhorse@users.noreply.github.com>
2020-05-05 08:58:18 -0500

    Este es el sudo de la paguina bookPlus

--- sudo.php
+++ sudo.php
@@ -2,6 +2,32 @@
 <html>
 
 <head>
+	<style>
+		#container{
+			width: 90%;
+			height: 90%;
+			float: left;
+		}
+		#imagenLibro{
+			width: 40%;
+			height: 180px;
+			margin-left: 30%;
+			margin-right: 30%;
+			float: left;
+		}
+
+        .dat{
+            width: 40%;
+            height: 10px;
+            margin-top: 10%;
+            margin-left: 5%;
+            color: black;
+            font-size: 100%;
+            text-align: center;
+            float: left;
+        }
+    
+    </style>
     <title> SuperAdmin </title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
@@ -42,12 +68,17 @@
             <div id="buscador">
                 <button id="todoSite">BUSQUEDA</button>
                 <input id="inputBuscar" type="" name="buscador">
-                <button id="botonLupa"><img src="files/Icons/lupa.png" width="100%" alt="lupa">
+                <button id="botonLupa"><img src="files/lupa.png" width="100%" alt="lupa">
                 </button>
             </div>
             <div id="usuario" style="margin:0.5% 20% 0 0 ; float:right;">
-                <img src="files/Icons/Icono_Usuario.png" width="40%" alt="usuario">
-                <li class="letraOlaDos" ><a href="index.html"> Bienvenido</a> </li>
+                <img src="files/icono_Usuario.png" width="40%" alt="usuario">
+                <li class="letraOlaDos" id="usuario1" >
+                <script>
+                    nombre = sessionStorage.getItem('usuario');
+                    document.getElementById("usuario1").innerHTML = nombre;
+                </script>   
+                </li>
                 <li class="letraOla" ><a href="index.html"> Salir </a> </li>
             </div>
         </div>
@@ -66,7 +97,7 @@
                             <li><a href="#consultarProd"> Consultar Producto </a> </li>
                             <li><a href="#agregarProd"> Agregar Producto </a> </li>
                             <li><a href="#actuProd"> Actualizar Producto </a> </li>
-                            <li><a href="#estadoProd"> Estado Producto </a> </li>
+                            <li><a href="#eliminarPro"> Eliminar Producto </a> </li>
                         </ul>
                     </li>
                     <li style="margin: 0 3% 0 8%;"><a href="#categoria"> Categoria </a>
@@ -103,7 +134,7 @@
 
             <div class="camposDos" id="consultarProd">
                 <h1>Consulta de Libros</h1>
-                <form method="POST" action="#"><br>
+                <form method="POST" action="control/libroControl.php?accion=consultar"><br>
                     <input type="text" placeholder="Nombre libro a buscar" class="datos" name="buscar" />
                     <br>
                     <br>
@@ -112,58 +143,168 @@
                     <div id="result"></div>
                 </form>
 
-                <div class="mostrarLibro" style="width: 500px; height:500px; background:black; margin: 20% 40% 0 -35%">
-                   
+                <div class="mostrarLibro" id="busquedaLibro" style="width: 500px; height:500px; background:#FFFFFF; margin: 20% 35% 0 -35%">
+                   <script>    
+                    idLibro = localStorage.getItem('id');                                                      
+                    imag =  localStorage.getItem('imagen');
+                    nomb = localStorage.getItem('nombre');
+                    aut = localStorage.getItem('autor');
+                    edit = localStorage.getItem('editorial');
+                    existencia = localStorage.getItem('existencias');
+                    prec = localStorage.getItem('precio');
+                    cat = localStorage.getItem('idCat');
+                    suCat = localStorage.getItem('idSub');
+                    estado = localStorage.getItem('estado');
+                                   
+                    container = document.createElement("div");
+                    card = document.createElement('div');
+
+                    img = document.createElement("img");
+                    // label y contenido del la inf del id
+                    labId = document.createElement('label');
+                    cLabelImg = document.createTextNode("idLibro = ");
+                    labId.appendChild(cLabelImg); // valor del label id
+                    idL= document.createElement('div');
+                    numId = document.createTextNode(idLibro);
+                    idL.appendChild(labId);
+                    idL.appendChild(numId);
+                    //  label y contenido del nombre
+                    labNo = document.createElement('label');
+                    cLabelNo = document.createTextNode("nombre = ");
+                    labNo.appendChild(cLabelNo); // valor del label nombre
+                    nomLibro = document.createElement("div");
+                    contnom = document.createTextNode(nomb);
+                    nomLibro.appendChild(labNo);
+                    nomLibro.appendChild(contnom);
+                    // label y contenido del autor
+                    labAut = document.createElement('label');
+                    cLabelAut = document.createTextNode("autor = ");
+                    labAut.appendChild(cLabelAut); // valor del label autor
+                    nomAut = document.createElement("div");
+                    contAut = document.createTextNode(aut);
+                    nomAut.appendChild(labAut);
+                    nomAut.appendChild(contAut);
+                    // label y contenido de la editorial
+                    labedi = document.createElement('label');
+                    cLabeledi = document.createTextNode("editorial = ");
+                    labedi.appendChild(cLabeledi); // valor del label editorial
+                    nomEdi = document.createElement("div");
+                    contEdi = document.createTextNode(edit);
+                    nomEdi.appendChild(labedi);
+                    nomEdi.appendChild(contEdi);
+                    // label y contenido de existencias
+                    labexi = document.createElement('label');
+                    cLabelexi = document.createTextNode("existencias = ");
+                    labexi.appendChild(cLabelexi); // valor del label existencias
+                    exis = document.createElement("div");
+                    contExis = document.createTextNode(existencia);
+                    exis.appendChild(labexi);
+                    exis.appendChild(contExis);
+                    // label y contenido de precio
+                    labpre = document.createElement('label');
+                    cLabelpre = document.createTextNode("precio = ");
+                    labpre.appendChild(cLabelpre); // valor del label precio
+                    precio = document.createElement("div");
+                    valor = document.createTextNode(prec);
+                    precio.appendChild(labpre);
+                    precio.appendChild(valor);
+                    // label y contenido idCategoria
+                    labcat = document.createElement('label');
+                    cLabelcat = document.createTextNode("idCategoria = ");
+                    labcat.appendChild(cLabelcat); // valor del label categoria
+                    categoria = document.createElement("div");
+                    catValor = document.createTextNode(cat);
+                    categoria.appendChild(labcat);
+                    categoria.appendChild(catValor);
+                    // label y contenido idsubCategoria
+                    labScat = document.createElement('label');
+                    cLabelScat = document.createTextNode("idSubcategoria = ");
+                    labScat.appendChild(cLabelScat); // valor del label subcategoria
+                    subCat = document.createElement("div");
+                    subCatValor = document.createTextNode(cat);
+                    subCat.appendChild(labScat);
+                    subCat.appendChild(subCatValor);
+                    // label y contenido estado
+                    labest = document.createElement('label');
+                    cLabelest = document.createTextNode("idEstado = ");
+                    labest.appendChild(cLabelest); // valor del label estado
+                    estado = document.createElement("div");
+                    estadoValor = document.createTextNode(cat);
+                    estado.appendChild(labest);
+                    estado.appendChild(estadoValor);
+                    // colocamos cada uno de los elemento en la card
+                    card.appendChild(img);
+                    card.appendChild(idL);
+                    card.appendChild(nomLibro);
+                    card.appendChild(nomAut);
+                    card.appendChild(nomEdi);
+                    card.appendChild(exis);
+                    card.appendChild(precio);
+                    card.appendChild(categoria);
+                    card.appendChild(subCat);
+                    card.appendChild(estado);
+                    // posicionamos la card
+                    container.appendChild(card);
+                    // atributos del contenedor de mi card
+                    container.setAttribute("id","container");
+                    img.setAttribute("id","imagenLibro");
+                    img.setAttribute("src",imag);
+                    img.setAttribute("alt","libro");
+                    idL.setAttribute('class',"dat");
+                    nomLibro.setAttribute("class","dat");
+                    nomAut.setAttribute("class","dat");
+                    nomEdi.setAttribute("class","dat");
+                    exis.setAttribute("class","dat");
+                    precio.setAttribute("class","dat");
+                    categoria.setAttribute("class","dat");
+                    subCat.setAttribute("class","dat");
+                    estado.setAttribute("class","dat");
+
+                    document.getElementById("busquedaLibro").appendChild(container);                               
+                           
+                </script>                    
                 </div>
             </div>
 
-            <div class="campos" id="agregarProd">
-                <h1>Agregar un nuevo libro</h1>
-                <form method="POST" action="" enctype="multipart/form-data">><br>
-                    <input type="text" placeholder="Nombre del libro" class="datos" name="nombreProd" /><br>
-                    <input type="text" placeholder="Autor del libro" class="datos" name="autorProd" /><br>
-                    <input type="text" placeholder="Editorial del libro" class="datos" name="editorialProd" /><br>
-                    <input type="text" placeholder="Existencias del libro" class="datos" name="cantProd" /><br>
-                    <input type="text" placeholder="Precio del libro" class="datos" name="precioProd" /><br>
-                    <input type="text" placeholder="Categoria del libro" class="datos" name="catProd" /><br>
-                    <input type="text" placeholder="Subcategoria del libro" class="datos" name="subcatProd" /><br>
-                    <input type="text" placeholder="Estado del libro" class="datos" name="estadoProd" /><br>
-                    <input type="file" name="imagen" class="datos"><br>
-                    <button class="btnForm" type="submit">Enviar</button>
-                    <br>
-                </form>
-            </div>
+           <div class="campos" id="agregarProd">
+                      <h1>Agregar un nuevo libro</h1>
+                       <form method="POST" action="control/libroControl.php?accion=nuevo" enctype="multipart/form-data"><br>
+                          <input type="text" placeholder="Nombre del libro" class="datos" name="nombre" ><br>
+                          <input type="text" placeholder="Autor del libro" class="datos" name="autor" ><br>
+                          <input type="text" placeholder="Editorial del libro" class="datos" name="editor" ><br>
+                          <input type="text" placeholder="Existencias del libro" class="datos" name="cantidad" ><br>
+                          <input type="text" placeholder="Precio del libro" class="datos" name="precio" ><br>
+                          <input type="text" placeholder="Categoria del libro" class="datos" name="categoria" ><br>
+                          <input type="text" placeholder="Subcategoria del libro" class="datos" name="subcategoria" ><br>
+                          <input type="text" placeholder="Estado del libro" class="datos" name="estado" ><br>
+                          <input type="file" name="imagen" class="datos">  <br> 
+                          <button class="btnForm" type="submit">Enviar</button>
+                          <br>
+                       </form>
+                  </div>
 
             <div class="campos" id="actuProd">
                 <h1>Actualizar Libro</h1>
-                <form method="POST" action="#" enctype="multipart/form-data">><br>
-                    <input type="text" placeholder="Nombre del libro" class="datos" name="nombreBuscar" /><br>
-                    <input type="text" placeholder="Autor del libro" class="datos" name="autorProd" /><br>
-                    <input type="text" placeholder="Editorial del libro" class="datos" name="editorialProd" /><br>
-                    <input type="text" placeholder="Existencias del libro" class="datos" name="cantProd" /><br>
-                    <input type="text" placeholder="Nuevo precio del libro" class="datos" name="nuevoPrecio" /><br>
-                    <input type="text" placeholder="Categoria del libro" class="datos" name="catProd" /><br>
-                    <input type="text" placeholder="Subcategoria del libro" class="datos" name="subCatProd" /><br>
-                    <input type="text" placeholder="Estado del libro" class="datos" name="estadoProd" /><br>
-                    <input type="file" name="imagen" class="datos"><br>
+                <form method="POST" action="control/libroControl.php?accion=modificar" enctype="multipart/form-data"><br>
+                    <input type="text" placeholder="id del libro" class="datos" name="id" /><br>
+                    <input type="text" placeholder="campo a modificar" class="datos" name="nombreColumna" /><br>
+                    <input type="text" placeholder="valor" class="datos" name="valor" /><br>
                     <button class="btnForm" style="width: 35% !important;" type="submit">Actualizar</button>
                     <br>
                 </form>
             </div>
-
-            <div class="camposDos" id="estadoProd">
-                <h1>Activar o desactivar un producto</h1>
-                <form method="POST" action="#"><br>
-                    <input type="number" placeholder="Id producto" class="datos" name="idProd" /><br>
-                    <h3>Activar</h3>
-                    <input type="text" placeholder="Escriba activar" class="datos" name="activarProd" /><br>
-                    <h3>Desactivar</h3>
-                    <input type="text" placeholder="Escriba desactivar" class="datos" name="desactivarProd" /><br>
-                    <button class="btnForm" type="submit">Enviar</button>
+ 
+            <div class="campos" id="eliminarPro">
+                <h1>Eliminar libro</h1>
+                <form method="POST" action="control/libroControl.php?accion=eliminar" ><br>
+                    <input type="text" placeholder="id del libro" class="datos" name="id" /><br>
+                    <button class="btnForm" style="width: 35% !important;" type="submit">eliminar</button>
+                    <br>
                 </form>
             </div>
+ 
 
-            <!--                             Administrador                      -->
+            <!------------------------------------------------------Administrador---------------------------------------------------->
 
 
             <div class="campos" id="admin">
@@ -183,11 +324,11 @@
                 <br>
 
                 <h1>Agregar un nuevo Admininistrador</h1>
-                <form method="POST" action="#"><br>
-                    <input type="text" placeholder="Nombre del Administrador" class="datos" name="nombreAdmin" /><br>
-                    <input type="text" placeholder="apellido del Administrador" class="datos" name="apellidoAdmin" /><br>
-                    <input type="text" placeholder="Correo del Administrador" class="datos" name="correoAdmin" /><br>
-                    <input type="text" placeholder="Telefono del Administrador" class="datos" name="telefonoAdmin" /><br>
+                <form method="POST" action="control/libroControl.php?accion=nuevoAdm"><br>
+                    <input type="text" placeholder="Nombre del Administrador" class="datos" name="nombre" /><br>
+                    <input type="text" placeholder="apellido del Administrador" class="datos" name="apellido" /><br>
+                    <input type="text" placeholder="Correo del Administrador" class="datos" name="correo" /><br>
+                    <input type="text" placeholder="Telefono del Administrador" class="datos" name="telefono" /><br>
                     <button class="btnForm" type="submit">Enviar</button>
                 </form>
 
@@ -207,11 +348,11 @@
             </div>
 
 
-            <!--                        Categoria                   -->
+            <!------------------------------------------------------------------Categoria -------------------------------------------------->
 
             <div class="campos" id="categoria">
                 <h1>Consultar una categoria</h1>
-                <form method="POST" action="#"><br>
+                <form method="POST" action=""><br>
                     <input type="number" placeholder="Id categoria a buscar" class="datos" name="buscarCat" />
                     <button class="btnForm" type="submit">Buscar</button>
                     <div id="result"></div>
@@ -226,8 +367,9 @@
                 <br>
 
                 <h1>Agregar una nueva categoria</h1>
-                <form method="POST" action="#"><br>
-                    <input type="text" placeholder="Nombre nuevo categoria" class="datos" name="nombreCat" /><br>
+                <form method="POST" action="control/libroControl.php?accion=nuevaCategoria"><br>
+                    <input type="text" placeholder="Nombre nuevo categoria" class="datos" name="nombre" /><br>
+                    <input type="text" placeholder="estado de la categoria" class="datos" name="estado" /><br>
                     <button class="btnForm" type="submit">Enviar</button>
                     <br>
                 </form>
@@ -259,7 +401,7 @@
 
             </div>
 
-            <!--                        Subcategoria                   -->
+            <!--------------------------------------------------------------Subcategoria--------------------------------------------------------->
 
             <div class="campos" id="subcategoria">
                 <h1>Consultar una subcategoria</h1>
