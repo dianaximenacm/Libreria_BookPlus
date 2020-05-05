58ddcd9656f5bf6479ddefb373da2dc39aef4a42
mainhorse <52118001+mainhorse@users.noreply.github.com>
2020-05-05 09:00:02 -0500

    modificacion de la parte de control del producto libro

--- control/libroControl.php
+++ control/libroControl.php
@@ -1,59 +1,94 @@
 <?php
 
+require( __DIR__ . '/../modelo/class.Libro.php');
 $accion = $_GET['accion'];
 
 switch($accion){
     case 'nuevo':
                  nuevoProducto();
     break;
+    case 'consultar':
+                consultar();
+    break;    
     case 'modificar':
                  modificarProducto();
     break;
-    case 'categoria':
-                 categoria();
+    case 'eliminar':
+                eliminar();
+    break;   
+    case 'nuevoAdm':
+                nuevoAdm();
+    break;    
+    case 'nuevaCategoria':
+                 nuevacategoria();
     break;
     case 'subCategoria':
                  subCategoria();
-    break;        
-    
+    break;           
 }
 
 function nuevoProducto(){
-    $producto = new Producto();
-    $nombre = $_POST[''];
-    $autor = $_POST[''];
-    $editorial = $_POST[''];
-    $existencias = $_POST[''];
-    $precio = $_POST[''];
-    $cate = $_POST[''];
-    $subCate = $_POST[''];
-    $estado = $_POST[''];
-    $result = $producto->nuevoProducto($nombre,$autor,$editorial,$existencias,$precio,$cate,$subCate,$estado);
+    $libro = new Libro();
+    $imagen = $_POST['imagen'];
+    $nombre = $_POST['nombre'];
+    $autor = $_POST['autor'];
+    $edit = $_POST['editor'];
+    $exi = $_POST['cantidad'];
+    $precio = $_POST['precio'];
+    $cate = $_POST['categoria'];
+    $subCate = $_POST['subcategoria'];
+    $estado = $_POST['estado'];
+    $libro->nuevoProducto($imagen,$nombre,$autor,$edit,$exi,$precio,$cate,$subCate,$estado);
     header('location: ../sudo.php?value=ProductoNuevo');
 }
 
+function consultar(){
+    $libro = new Libro();
+    $busqueda = $_POST['buscar'];
+    $libro->consulta($busqueda);
+   
+}
+
 function modificarProducto(){
-    $producto = new Producto();
-    $idProducto = $_POST[''];
-    $columnaProducto = $_POST[''];
-    $nuevoValor = $_POST[''];
-    $producto->modificarProducto($idProducto,$columnaProducto,$nuevoValor);
+    $libro = new Libro();
+    $idProducto = $_POST['id'];
+    $columnaProducto = $_POST['nombreColumna'];
+    $nuevoValor = $_POST['valor'];
+    $libro->modificarProducto($idProducto,$columnaProducto,$nuevoValor);
     header('location: ../sudo.php?value=modificarProducto');
 
 }
 
-function categoria(){
-    $producto = new Producto();
-    $nueCategoria = $_POST[''];
-    $estado = $_POST[''];
-    $producto->agregarCategoria($nueCategoria,$estado);
+function eliminar(){
+    $libro = new Libro();
+    $fila = $_POST['id'];
+    $libro->eliminar($fila);
+    header('location: ../sudo.php?value=productoEliminado');
+}
+
+function nuevoAdm(){
+    $libro = new Libro();
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $correo = $_POST['correo'];
+    $telefono = $_POST['telefono'];
+    $libro -> nuevoAdm($nombre,$apellido,$correo,$telefono);
+    header('location: ../sudo.php?value=nuevoAdministrador');
+}
+
+function nuevacategoria(){
+    $libro = new Libro();
+    $nueCategoria = $_POST['nombre'];
+    $estado = $_POST['estado'];
+    $libro->agregarCategoria($nueCategoria,$estado);
+    header('location: ../sudo.php?value=nuevaCategoria');
 }
 
 function subCategoria(){
-    $producto = new Producto();
+    $libro = new Libro();
     $nueCategoria = $_POST[''];
     $estado = $_POST[''];
-    $producto->agregarsubCategoria($nueCategoria,$estado);
+    $libro->agregarsubCategoria($nueCategoria,$estado);
 }
 
 ?>
